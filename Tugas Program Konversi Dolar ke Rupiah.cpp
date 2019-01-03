80d54395d847f3e120b5ecb71bd1866e96546110
Muhamad-Andriansyah <45659337+Muhamad-Andriansyah@users.noreply.github.com>
2019-01-03 13:24:46 +0700

    Add files via upload

---
+++ Tugas Program Konversi Dolar ke Rupiah.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "KONVERSI DOLAR KE RUPIAH - MUHAMAD ANDRIANSYAH (NIM 311810297) TI-18 B2" <<endl;
+
+    int Rp, dollar;
+    cout<<"masukan nilai yang ingin di konversi ke rupiah : ";
+    cin>>dollar;
+    Rp = 15149 * dollar;
+    cout<<"hasil konversi dollar ke rupiah = " <<Rp;
+    return 0;
+}
